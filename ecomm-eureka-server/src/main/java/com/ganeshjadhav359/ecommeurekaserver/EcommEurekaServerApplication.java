03c060d14c816325212f1bf63b1638e4b2104601
ganeshjadhav359 <ganeshjadhav359@gmail.com>
2020-09-26 00:36:38 +0530

    eureka server code added

---
+++ ecomm-eureka-server/src/main/java/com/ganeshjadhav359/ecommeurekaserver/EcommEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ganeshjadhav359.ecommeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EcommEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommEurekaServerApplication.class, args);
+	}
+
+}
